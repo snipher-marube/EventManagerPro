7fdd8768bd29440958092e44a834f82c529adc86
snipher <sniphermarube@gmail.com>
2024-06-26 14:34:01 +0300

    vercel deployment

--- eventmanager/settings/production.py
+++ eventmanager/settings/production.py
@@ -17,7 +17,7 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 DEBUG = False
 
 # Allowed hosts
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv(), default=['event-manager-pro.vercel.app'])
+ALLOWED_HOSTS = ['event-manager-pro.vercel.app']
 
 # Static and media files settings
 STATIC_URL = '/static/'
