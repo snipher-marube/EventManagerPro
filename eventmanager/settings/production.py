d074f771cebb03f6098ece251154ebd2a1e63e1a
snipher <sniphermarube@gmail.com>
2024-06-26 10:48:12 +0300

    vercel deployment

--- eventmanager/settings/production.py
+++ eventmanager/settings/production.py
@@ -4,17 +4,7 @@ import dj_database_url
 
 # Replace the DATABASES section of your settings.py with this
 DATABASES = {
-  'default': {
-    'ENGINE': 'django.db.backends.postgresql',
-    'NAME': config('PGDATABASE'),
-    'USER': config('PGUSER'),
-    'PASSWORD': config('PGPASSWORD'),
-    'HOST': config('PGHOST'),
-    'PORT': config('PGPORT', 5432),
-    'OPTIONS': {
-      'sslmode': 'require',
-    },
-  }
+    'default': dj_database_url.config(default=config('DATABASE_URL'))
 }
 DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 DATABASES['default']['CONN_MAX_AGE'] = 60
